b0b2ba0c6b2d1982f837506cf9a93a5fd0452f73
Resul Elezi <resul_elezi@protonmail.com>
2024-10-21 10:07:05 +0200

    #

--- js/script.js
+++ js/script.js
@@ -6,4 +6,16 @@ const INIT_APP = () => {
     const ACTIVE = () => DROP_AREA.classList.add("gree-border");
     const INACTIVE = () => DROP_AREA.classList.remove("gree-border");
     const PREVENTS = (e) => e.preventDefault();
+
+    ["dragover", "drop"].forEach(evtName => {
+        DROP_AREA.addEventListener(evtName, PREVENTS);
+    });
+    ["dragover", "drop"].forEach(evtName => {
+        DROP_AREA.addEventListener(evtName, ACTIVE);
+    });
+    ["dragover", "drop"].forEach(evtName => {
+        DROP_AREA.addEventListener(evtName, INACTIVE);
+    });
+
+    DROP_AREA.addEventListener("drop", handleDrop);
 }
